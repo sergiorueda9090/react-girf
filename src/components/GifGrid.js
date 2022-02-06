c46dec9ca2dead8308c847bc6040038d2333c4d4
sergio rueda <srueda@smartdataautomation.com>
2022-02-06 07:25:44 -0500

    First commit

---
+++ src/components/GifGrid.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useFetchGifs } from '../hooks/useFetchGifs';
+import { GifGridItem } from './GifGridItem';
+
+export const GifGrid = ({ category }) => {
+
+        const { data, loading } = useFetchGifs(category);
+        console.log("data ", data);
+        return ( < >
+                <
+                h3 className = "animate__animated animate__bounce" > { category } < /h3> {
+                loading && < p className = "animate__animated animate__flash" > Loading < /p>} <
+                div className = "card-grid" > {
+                    data.map(img => ( < GifGridItem key = { img.id } {...img }
+                            />))}  < /
+                            div > <
+                            />)
+                        }
